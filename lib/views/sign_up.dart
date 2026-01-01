aaa108da9324740447c9ca19394832ded844fbab
AATHILA <athilashahin@gmail.com>
2026-01-01 14:55:17 +0530

    sign up page created

--- lib/views/sign_up.dart
+++ lib/views/sign_up.dart
@@ -1,15 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../controllers/auth/auth_bloc/auth_bloc.dart';
 
-class SignUp extends StatefulWidget {
-  const SignUp({super.key});
+class SignUp extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
-  @override
-  State<SignUp> createState() => _SignUpState();
-}
+  SignUp({super.key});
 
-class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Create Account")),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthAuthenticated) {
+
+            Navigator.pop(context);
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Account created successfully!")),
+            );
+          } else if (state is AuthError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.message), backgroundColor: Colors.red),
+            );
+          }
+        },
+        builder: (context, state) {
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(labelText: "Email"),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                TextField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(labelText: "Password"),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 24),
+                state is AuthLoading
+                    ? const CircularProgressIndicator()
+                    : ElevatedButton(
+                  onPressed: () {
+                    context.read<AuthBloc>().add(
+                      SignUpRequested(
+                        emailController.text.trim(),
+                        passwordController.text.trim(),
+                      ),
+                    );
+                  },
+                  child: const Text("Sign Up"),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
